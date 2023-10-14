ced305ebc090bfe890d62ca4e5ed0af612063cb1
Robin Williams <56467060+robinawilliams@users.noreply.github.com>
2023-10-14 06:39:11 -0500

    file operations, ui fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 from tkinter import filedialog
 import json
+import send2trash  # Import the send2trash library
 
 
 class FileRenamerApp:
@@ -14,6 +15,11 @@ class FileRenamerApp:
         self.queue = []
 
         self.create_gui()
+
+        # Add the last used file tracking variable
+        self.last_used_file = ""
+
+        self.categories = self.load_categories()
 
     def create_gui(self):
         # File Label
@@ -33,17 +39,17 @@ class FileRenamerApp:
         self.button_frame.pack()
 
         self.categories = self.load_categories()  # Load categories from a configuration file
-        self.categories.sort()  # Sort the categories alphabetically
+        self.categories.sort(key=lambda x: x.lower())
 
         self.buttons = []
         row = 0
         col = 0
         for category in self.categories:
-            button = tk.Button(self.button_frame, text=category, command=lambda g=category: self.add_to_queue(g))
+            button = tk.Button(self.button_frame, text=category, command=lambda c=category: self.add_to_queue(c))
             button.grid(row=row, column=col, padx=5, pady=5)
             self.buttons.append(button)
             col += 1
-            if col == 4:
+            if col == 6:
                 col = 0
                 row += 1
 
@@ -58,6 +64,14 @@ class FileRenamerApp:
         self.remove_category_entry.pack()
         self.remove_category_button = tk.Button(self.root, text="Remove Category", command=self.remove_category)
         self.remove_category_button.pack()
+
+        # Custom Text Entry Label
+        self.custom_text_label = tk.Label(self.root, text="Custom text entry:")
+        self.custom_text_label.pack()
+
+        # Custom Text Entry
+        self.custom_text_entry = tk.Entry(self.root)
+        self.custom_text_entry.pack()
 
         # Rename Button
         self.rename_button = tk.Button(self.root, text="Rename Files", command=self.rename_files)
@@ -83,6 +97,34 @@ class FileRenamerApp:
         self.root.drop_target_register(DND_FILES)
         self.root.dnd_bind('<<Drop>>', self.on_drop)
 
+        self.trash_button = tk.Button(self.root, text="Move to Trash", command=self.move_to_trash)
+        self.trash_button.pack()
+
+        # Last Used File Button
+        self.last_used_file_button = tk.Button(self.root, text="Select Last Used File",
+                                               command=self.load_last_used_file)
+        self.last_used_file_button.pack()
+
+    def move_to_trash(self):
+        if self.selected_file:
+            confirmation = tk.messagebox.askyesno("Confirm Action",
+                                                  "Are you sure you want to move this file to the trash?")
+            if confirmation:
+                send2trash.send2trash(self.selected_file)
+                self.selected_file = ""
+                self.queue = []
+                self.file_display.config(text="")
+                self.custom_text_entry.delete(0, tk.END)
+                self.show_message("File moved to trash successfully")
+
+    def load_last_used_file(self):
+        if self.last_used_file:
+            self.selected_file = self.last_used_file
+            self.file_display.config(text=self.selected_file)
+            self.queue = []
+            self.update_file_display()
+            self.show_message("Last used file selected: " + self.selected_file)
+
     def on_drop(self, event):
         # Get the dropped file path without curly braces
         self.selected_file = event.data.strip('{}')
@@ -99,7 +141,8 @@ class FileRenamerApp:
 
     def update_file_display(self):
         if self.selected_file:
-            new_name = os.path.splitext(self.selected_file)[0] + " " + " ".join(self.queue) + \
+            custom_text = self.custom_text_entry.get().strip()
+            new_name = os.path.splitext(self.selected_file)[0] + " " + custom_text + " " + " ".join(self.queue) + \
                        os.path.splitext(self.selected_file)[1]
             self.file_display.config(text=new_name)
 
@@ -142,11 +185,11 @@ class FileRenamerApp:
         row = 0
         col = 0
         for category in self.categories:
-            button = tk.Button(self.button_frame, text=category, command=lambda g=category: self.add_to_queue(g))
+            button = tk.Button(self.button_frame, text=category, command=lambda c=category: self.add_to_queue(c))
             button.grid(row=row, column=col, padx=5, pady=5)
             self.buttons.append(button)
             col += 1
-            if col == 4:
+            if col == 6:
                 col = 0
                 row += 1
 
@@ -162,14 +205,17 @@ class FileRenamerApp:
             json.dump(self.categories, file)
 
     def rename_files(self):
-        if self.selected_file and self.queue:
-            new_name = os.path.splitext(self.selected_file)[0] + " " + " ".join(self.queue) + \
+        if self.selected_file and (self.queue or self.custom_text_entry.get().strip()):
+            custom_text = self.custom_text_entry.get().strip()
+            new_name = os.path.splitext(self.selected_file)[0] + " " + custom_text + " " + " ".join(self.queue) + \
                        os.path.splitext(self.selected_file)[1]
             try:
                 os.rename(self.selected_file, new_name)
                 self.selected_file = ""
                 self.queue = []
                 self.file_display.config(text="")
+                self.custom_text_entry.delete(0, tk.END)
+                self.last_used_file = new_name  # Set the last used file
                 self.show_message("File renamed successfully")
             except OSError as e:
                 self.show_message("Error: " + str(e), error=True)
