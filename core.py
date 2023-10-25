fa7a39864a66ee67141292baefb6daac7dbb54fd
Robin Williams <56467060+robinawilliams@users.noreply.github.com>
2023-10-25 17:33:30 -0500

    code split

---
+++ core.py
@@ -0,0 +1,243 @@
+import os
+import re
+import json
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk, END
+from tkinterdnd2 import DND_FILES, TkinterDnD
+import send2trash
+import subprocess
+import platform
+
+# Constants
+INITIAL_DIRECTORY = "/path/to/your/starting/directory"  # Change this to your desired initial directory
+CATEGORIES_FILE = "categories.json"
+
+
+def browse_output_directory(self):
+    output_directory = filedialog.askdirectory(initialdir=INITIAL_DIRECTORY)
+
+    if output_directory:
+        self.output_directory = output_directory
+        self.output_directory_entry.delete(0, tk.END)
+        self.output_directory_entry.insert(0, self.output_directory)
+
+
+def move_to_trash(self):
+    if self.selected_file:
+        confirmation = messagebox.askyesno("Confirm Action",
+                                           "Are you sure you want to move this file to the trash?")
+        if confirmation:
+            send2trash.send2trash(self.selected_file)
+            self.selected_file = ""
+            self.queue = []
+            self.file_display.config(text="")
+            self.custom_text_entry.delete(0, tk.END)
+            self.show_message("File moved to trash successfully")
+
+
+def load_last_used_file(self):
+    if self.last_used_file:
+        self.selected_file = self.last_used_file
+        self.file_display.config(text=os.path.basename(self.selected_file))
+        self.queue = []
+        self.update_file_display()
+        self.show_message("Last used file selected: " + os.path.basename(self.selected_file))
+
+
+def on_drop(self, event):
+    self.selected_file = event.data.strip('{}')
+    self.file_display.config(text=os.path.basename(self.selected_file))  # Display only the filename
+    self.queue = []
+    self.update_file_display()
+    self.show_message("File selected: " + os.path.basename(self.selected_file))  # Update the message
+
+    if self.open_on_drop_var.get():
+        if platform.system() == "Linux":
+            # Open the dropped file using xdg-open on Linux
+            subprocess.Popen(['xdg-open', self.selected_file])
+        elif platform.system() == "Windows":
+            # Open the dropped file using the Windows start command. shell=True
+            subprocess.Popen(['start', self.selected_file])
+        else:
+            # Use 'open' command on macOS, you can customize this as needed
+            subprocess.Popen(['open', self.selected_file])
+
+
+def add_to_queue(self, category):
+    if self.selected_file:
+        if category in self.weights:
+            self.queue.insert(self.weights[category] - 1, category)
+        else:
+            self.queue.append(category)
+
+        self.update_file_display()
+        self.show_message("Word added: " + category)
+
+
+def update_file_display(self):
+    if self.selected_file:
+        custom_text = self.custom_text_entry.get().strip()
+        new_name = os.path.splitext(self.selected_file)[0] + " " + custom_text + " " + " ".join(self.queue) + \
+            os.path.splitext(self.selected_file)[1]
+
+        # Remove double spaces and trailing spaces
+        new_name = " ".join(new_name.split())  # Remove double spaces
+        new_name = new_name.strip()  # Remove trailing spaces
+
+        self.file_display.config(text=os.path.basename(new_name))
+
+
+def undo_last(self):
+    if self.queue:
+        self.queue.pop()
+        self.update_file_display()
+        self.show_message("Last category removed")
+
+
+def clear_selection(self):
+    self.selected_file = ""
+    self.queue = []
+    self.file_display.config(text="")
+    self.show_message("Selection cleared")
+
+
+def add_category(self):
+    new_category = self.category_entry.get().strip()
+    if new_category:
+        self.categories.append(new_category)
+        self.categories.sort()  # Sort the categories alphabetically
+        self.save_categories()
+        self.refresh_category_buttons()
+        self.category_entry.delete(0, tk.END)
+        self.show_message("Category added: " + new_category)
+
+
+def remove_category(self):
+    category_to_remove = self.remove_category_entry.get().strip()
+    if category_to_remove in self.categories:
+        self.categories.remove(category_to_remove)
+        self.save_categories()
+        self.refresh_category_buttons()
+        self.remove_category_entry.delete(0, tk.END)
+        self.show_message("Category removed: " + category_to_remove)
+
+
+def refresh_category_buttons(self):
+    for button in self.buttons:
+        button.destroy()
+
+    self.buttons = []
+    row = 0
+    col = 0
+    for category in self.categories:
+        button = tk.Button(self.button_frame, text=category, command=lambda c=category: self.add_to_queue(c))
+        button.grid(row=row, column=col, padx=5, pady=5)
+        self.buttons.append(button)
+        col += 1
+        if col == 8:
+            col = 0
+            row += 1
+
+
+def load_categories():
+    try:
+        with open(CATEGORIES_FILE, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def save_categories(self):
+    with open(CATEGORIES_FILE, "w") as file:
+        json.dump(self.categories, file)
+
+
+def rename_files(self):
+    if self.selected_file and (self.queue or self.custom_text_entry.get().strip()):
+        custom_text = self.custom_text_entry.get().strip()
+        base_name, extension = os.path.splitext(os.path.basename(self.selected_file))
+
+        if self.remove_duplicates_var.get():
+            self.queue = list(dict.fromkeys(self.queue))
+
+        weighted_categories = [category for category in self.queue if category in self.weights]
+        weighted_categories.sort(key=lambda category: self.weights[category])
+
+        new_name = self.construct_new_name(base_name, weighted_categories, custom_text, extension)
+
+        if self.move_text_var.get():
+            new_name = self.move_text(new_name)
+
+        new_name = self.sanitize_file_name(new_name)
+
+        if not self.output_directory:
+            self.output_directory = os.path.dirname(self.selected_file)
+
+        new_path = os.path.join(self.output_directory, os.path.basename(new_name))
+        try:
+            os.rename(self.selected_file, new_path)
+            self.handle_rename_success(new_path)
+        except OSError as e:
+            self.show_message("Error: " + str(e), error=True)
+
+
+def construct_new_name(self, base_name, weighted_categories, custom_text, extension):
+    # Construct the new name based on placement choice (prefix or suffix)
+    if self.placement_choice.get() == "prefix":
+        new_name = f"{custom_text} {base_name} {' '.join(weighted_categories)} {' '.join(self.queue)}"
+    else:  # Default to suffix
+        new_name = f"{base_name} {' '.join(weighted_categories)} {' '.join(self.queue)} {custom_text}"
+    return new_name + extension
+
+
+def move_text(self, name):
+    match = re.match(r"^(.*) - (.*?)__-__ (.*)\.(\w+)$", name)
+    if match:
+        prefix, moved_text, suffix, extension = match.groups()
+        name = f"{prefix} {suffix} {moved_text}.{extension}"
+    return name
+
+
+def sanitize_file_name(self, name):
+    # Remove double spaces and trailing spaces
+    return " ".join(name.split()).strip()
+
+
+def handle_rename_success(self, new_path):
+    self.selected_file = ""
+    self.queue = []
+    self.file_display.config(text="")
+    self.custom_text_entry.delete(0, tk.END)
+    self.last_used_file = new_path
+    self.last_used_display.config(text=os.path.basename(new_path))
+    self.show_message("File renamed and saved successfully")
+
+    if self.move_up_var.get():
+        # Move the file up one folder
+        parent_directory = os.path.dirname(os.path.dirname(new_path))
+        new_location = os.path.join(parent_directory, os.path.basename(new_path))
+        os.rename(new_path, new_location)
+        self.selected_file = new_location
+
+    if self.reset_output_directory_var.get():
+        # Clear and reset the Output Directory to the current directory
+        self.output_directory = os.path.dirname(self.selected_file)
+        self.output_directory_entry.delete(0, tk.END)
+        self.output_directory_entry.insert(0, self.output_directory)
+
+
+def browse_file(self):
+    file_path = filedialog.askopenfilename(initialdir=INITIAL_DIRECTORY)
+    if file_path:
+        self.selected_file = file_path
+        self.file_display.config(text=self.selected_file)
+        self.queue = []  # Clear the queue when a new file is selected
+        self.update_file_display()
+        self.show_message("File selected: " + os.path.basename(self.selected_file))
+
+
+def show_message(self, message, error=False):
+    if error:
+        self.message_label.config(text=message, fg="red")
+    else:
+        self.message_label.config(text=message, fg="black")
--- gui.py
+++ gui.py
@@ -1,23 +1,19 @@
 import os
+import re
 import json
 import tkinter as tk
+from tkinter import filedialog, messagebox, ttk, END
 from tkinterdnd2 import DND_FILES, TkinterDnD
-from tkinter import filedialog, messagebox
 import send2trash
-from tkinter import ttk
-import re
 import subprocess
 import platform
-
-# Constants
-INITIAL_DIRECTORY = "/path/to/your/starting/directory"  # Change this to your desired initial directory
-CATEGORIES_FILE = "categories.json"
+import core
 
 
-class FileRenamerApp:
+class OCDFileRenamer:
     def __init__(self, root):
         self.root = root
-        self.root.title("File Renamer")
+        self.root.title("OCD File Renamer")
 
         self.selected_file = ""
         self.queue = []
@@ -38,9 +34,6 @@ class FileRenamerApp:
 
         # Variable to track whether to enable the reset output directory feature.
         self.reset_output_directory_var = tk.BooleanVar(value=False)  # Default state is not to reset
-
-        # Load categories from a configuration file
-        self.categories = self.load_categories()
 
         # Initialize output directory
         self.output_directory = ""
@@ -75,7 +68,7 @@ class FileRenamerApp:
         self.button_frame = tk.Frame(self.root)
         self.button_frame.pack()
 
-        self.categories = self.load_categories()  # Load categories from a configuration file
+        self.categories = core.load_categories()  # Load categories from a configuration file
         self.categories.sort(key=lambda x: x.lower())
 
         self.buttons = []
@@ -231,158 +224,52 @@ class FileRenamerApp:
         self.root.dnd_bind('<<Drop>>', self.on_drop)
 
     def browse_output_directory(self):
-        output_directory = filedialog.askdirectory(initialdir=INITIAL_DIRECTORY)
-
-        if output_directory:
-            self.output_directory = output_directory
-            self.output_directory_entry.delete(0, tk.END)
-            self.output_directory_entry.insert(0, self.output_directory)
+        core.browse_output_directory(self)
 
     def move_to_trash(self):
-        if self.selected_file:
-            confirmation = messagebox.askyesno("Confirm Action",
-                                               "Are you sure you want to move this file to the trash?")
-            if confirmation:
-                send2trash.send2trash(self.selected_file)
-                self.selected_file = ""
-                self.queue = []
-                self.file_display.config(text="")
-                self.custom_text_entry.delete(0, tk.END)
-                self.show_message("File moved to trash successfully")
+        core.move_to_trash(self)
 
     def load_last_used_file(self):
-        if self.last_used_file:
-            self.selected_file = self.last_used_file
-            self.file_display.config(text=os.path.basename(self.selected_file))
-            self.queue = []
-            self.update_file_display()
-            self.show_message("Last used file selected: " + os.path.basename(self.selected_file))
+        core.load_last_used_file(self)
 
     def on_drop(self, event):
-        self.selected_file = event.data.strip('{}')
-        self.file_display.config(text=os.path.basename(self.selected_file))  # Display only the filename
-        self.queue = []
-        self.update_file_display()
-        self.show_message("File selected: " + os.path.basename(self.selected_file))  # Update the message
-
-        if self.open_on_drop_var.get():
-            if platform.system() == "Linux":
-                # Open the dropped file using xdg-open on Linux
-                subprocess.Popen(['xdg-open', self.selected_file])
-            elif platform.system() == "Windows":
-                # Open the dropped file using the Windows start command. shell=True
-                subprocess.Popen(['start', self.selected_file])
-            else:
-                # Use 'open' command on macOS, you can customize this as needed
-                subprocess.Popen(['open', self.selected_file])
+        core.on_drop(self, event)
 
     def add_to_queue(self, category):
-        if self.selected_file:
-            if category in self.weights:
-                self.queue.insert(self.weights[category] - 1, category)
-            else:
-                self.queue.append(category)
-
-            self.update_file_display()
-            self.show_message("Category added: " + category)
+        core.add_to_queue(self, category)
 
     def update_file_display(self):
-        if self.selected_file:
-            custom_text = self.custom_text_entry.get().strip()
-            new_name = os.path.splitext(self.selected_file)[0] + " " + custom_text + " " + " ".join(self.queue) + \
-                os.path.splitext(self.selected_file)[1]
-
-            # Remove double spaces and trailing spaces
-            new_name = " ".join(new_name.split())  # Remove double spaces
-            new_name = new_name.strip()  # Remove trailing spaces
-
-            self.file_display.config(text=os.path.basename(new_name))
+        core.update_file_display(self)
 
     def undo_last(self):
-        if self.queue:
-            self.queue.pop()
-            self.update_file_display()
-            self.show_message("Last category removed")
+        core.undo_last(self)
 
     def clear_selection(self):
-        self.selected_file = ""
-        self.queue = []
-        self.file_display.config(text="")
-        self.show_message("Selection cleared")
+        core.clear_selection(self)
 
     def add_category(self):
-        new_category = self.category_entry.get().strip()
-        if new_category:
-            self.categories.append(new_category)
-            self.categories.sort()  # Sort the categories alphabetically
-            self.save_categories()
-            self.refresh_category_buttons()
-            self.category_entry.delete(0, tk.END)
-            self.show_message("Category added: " + new_category)
+        core.add_category(self)
 
     def remove_category(self):
-        category_to_remove = self.remove_category_entry.get().strip()
-        if category_to_remove in self.categories:
-            self.categories.remove(category_to_remove)
-            self.save_categories()
-            self.refresh_category_buttons()
-            self.remove_category_entry.delete(0, tk.END)
-            self.show_message("Category removed: " + category_to_remove)
+        core.remove_category(self)
 
     def refresh_category_buttons(self):
-        for button in self.buttons:
-            button.destroy()
-
-        self.buttons = []
-        row = 0
-        col = 0
-        for category in self.categories:
-            button = tk.Button(self.button_frame, text=category, command=lambda c=category: self.add_to_queue(c))
-            button.grid(row=row, column=col, padx=5, pady=5)
-            self.buttons.append(button)
-            col += 1
-            if col == 8:
-                col = 0
-                row += 1
+        core.refresh_category_buttons(self)
 
     def load_categories(self):
-        try:
-            with open(CATEGORIES_FILE, "r") as file:
-                return json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            return []
+        core.load_categories(self)
 
     def save_categories(self):
-        with open(CATEGORIES_FILE, "w") as file:
-            json.dump(self.categories, file)
+        core.save_categories(self)
 
     def rename_files(self):
-        if self.selected_file and (self.queue or self.custom_text_entry.get().strip()):
-            custom_text = self.custom_text_entry.get().strip()
-            base_name, extension = os.path.splitext(os.path.basename(self.selected_file))
+        core.rename_files(self)
 
-            if self.remove_duplicates_var.get():
-                self.queue = list(dict.fromkeys(self.queue))
+    def handle_rename_success(self, new_path):
+        core.handle_rename_success(self, new_path)
 
-            weighted_categories = [category for category in self.queue if category in self.weights]
-            weighted_categories.sort(key=lambda category: self.weights[category])
-
-            new_name = self.construct_new_name(base_name, weighted_categories, custom_text, extension)
-
-            if self.move_text_var.get():
-                new_name = self.move_text(new_name)
-
-            new_name = self.sanitize_file_name(new_name)
-
-            if not self.output_directory:
-                self.output_directory = os.path.dirname(self.selected_file)
-
-            new_path = os.path.join(self.output_directory, os.path.basename(new_name))
-            try:
-                os.rename(self.selected_file, new_path)
-                self.handle_rename_success(new_path)
-            except OSError as e:
-                self.show_message("Error: " + str(e), error=True)
+    def browse_file(self):
+        core.browse_file(self)
 
     def construct_new_name(self, base_name, weighted_categories, custom_text, extension):
         # Construct the new name based on placement choice (prefix or suffix)
@@ -403,37 +290,6 @@ class FileRenamerApp:
         # Remove double spaces and trailing spaces
         return " ".join(name.split()).strip()
 
-    def handle_rename_success(self, new_path):
-        self.selected_file = ""
-        self.queue = []
-        self.file_display.config(text="")
-        self.custom_text_entry.delete(0, tk.END)
-        self.last_used_file = new_path
-        self.last_used_display.config(text=os.path.basename(new_path))
-        self.show_message("File renamed and saved successfully")
-
-        if self.move_up_var.get():
-            # Move the file up one folder
-            parent_directory = os.path.dirname(os.path.dirname(new_path))
-            new_location = os.path.join(parent_directory, os.path.basename(new_path))
-            os.rename(new_path, new_location)
-            self.selected_file = new_location
-
-        if self.reset_output_directory_var.get():
-            # Clear and reset the Output Directory to the current directory
-            self.output_directory = os.path.dirname(self.selected_file)
-            self.output_directory_entry.delete(0, tk.END)
-            self.output_directory_entry.insert(0, self.output_directory)
-
-    def browse_file(self):
-        file_path = filedialog.askopenfilename(initialdir=INITIAL_DIRECTORY)
-        if file_path:
-            self.selected_file = file_path
-            self.file_display.config(text=self.selected_file)
-            self.queue = []  # Clear the queue when a new file is selected
-            self.update_file_display()
-            self.show_message("File selected: " + os.path.basename(self.selected_file))
-
     def show_message(self, message, error=False):
         if error:
             self.message_label.config(text=message, fg="red")
@@ -443,5 +299,5 @@ class FileRenamerApp:
 
 if __name__ == "__main__":
     root = TkinterDnD.Tk()
-    app = FileRenamerApp(root)
+    app = OCDFileRenamer(root)
     root.mainloop()
